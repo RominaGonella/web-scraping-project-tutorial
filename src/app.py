1af80c895e69ed010154ec88359613d3785903e8
RominaGonella <rominagonfur@gmail.com>
2022-06-24 01:28:31 +0000

    actualizo

--- src/app.py
+++ src/app.py
@@ -1,1 +1,16 @@
-# your app code here
+# your librerias
+import pandas as pd
+import sqlite3
+import requests
+from bs4 import BeautifulSoup
+
+# datos
+r = requests.get('https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue')
+#html_data = r.json()
+html_data = r.text
+print(html_data)
+
+# limpio
+data = BeautifulSoup(html_data, 'html.parser')
+data2 = data.prettify()
+print(data2)
